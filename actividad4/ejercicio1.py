87a5dc3cc8d1bfeca0144cbac9fec930eb0eb9e9
andyrueda439 <ajrueda@uamv.edu.ni>
2025-04-22 11:53:06 -0500

    Numero dentro de un rango

---
+++ actividad4/ejercicio1.py
@@ -0,0 +1,8 @@
+inicio = int(input("Dime el valor inicial: "))
+fin = int(input("Dime el valor final: "))
+
+numero = int (input("Dime un numero: "))
+
+if numero > inicio and numero < fin: 
+    print("El numero es valido")
+else: print("El numero es invalido")
