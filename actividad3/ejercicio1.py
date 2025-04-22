9df052960624384dee3724fbc3308685e1d572db
andyrueda439 <ajrueda@uamv.edu.ni>
2025-04-22 07:32:26 -0500

    Calculo de promedio

---
+++ actividad3/ejercicio1.py
@@ -0,0 +1,10 @@
+calificacion1 = int(input("Ingrese a calificacion 1: "))
+calificacion2 = int(input("Ingrese a calificacion 2: "))
+calificacion3 = int(input("Ingrese a calificacion 3: "))
+suma = calificacion1 + calificacion2 + calificacion3
+promedio = suma / 3
+
+print(f"""Calificacion 1: {calificacion2}
+Calificacion 2: {calificacion2}
+Calificacion 3: {calificacion3}
+Promedio {promedio:.0f}""")
