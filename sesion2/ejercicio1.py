2655f53adaf3961c127b9a96dd33848d6d697150
andyrueda439 <ajrueda@uamv.edu.ni>
2025-04-25 07:33:52 -0500

    Calificacion del 0-10

---
+++ sesion2/ejercicio1.py
@@ -0,0 +1,14 @@
+calificacion = float(input("Ingrese la calificacion del estudiante (0-10)"))
+
+if calificacion >= 9 and calificacion <= 10:
+    print("La calificacion es A")
+elif calificacion >= 7 and calificacion <= 8:
+    print("La calificacion es B")
+elif calificacion >= 5 and calificacion <= 6:
+    print("La calificacion es C")
+elif calificacion >= 3 and calificacion <= 4:
+    print("La calificacion es D")
+elif calificacion >= 0 and calificacion <= 2:
+    print("La calificacion es F")
+else:
+    print("Error. La calificacion no es valida.")
