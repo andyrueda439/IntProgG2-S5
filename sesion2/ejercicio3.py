513968c9e39b432dfcc534b5341417d58a220e77
andyrueda439 <ajrueda@uamv.edu.ni>
2025-04-25 08:38:48 -0500

    Numero de semana

---
+++ sesion2/ejercicio3.py
@@ -0,0 +1,18 @@
+numero_semana = int(input("Ingrese el numero que determine el dia de la semana: "))
+
+if numero_semana == 1:
+    print("Es lunes")
+elif numero_semana == 2: 
+    print("Es martes")
+elif numero_semana == 3:
+    print("Es miercoles")
+elif numero_semana == 4: 
+    print("Es jueves")
+elif numero_semana == 5: 
+    print("Es viernes")
+elif numero_semana == 6:  
+    print("Es sabado")
+elif numero_semana == 7:  
+    print("Es domingo")
+else:
+    print("El numero es invalido.")
